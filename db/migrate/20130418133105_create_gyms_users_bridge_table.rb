dcc84969e5365c865980a44c88b4f00ce2f400ea
Juan Pardini <jfpardini@gmail.com>
2013-04-24 11:23:40 +0100

    Fun with Sublime Text 2 and Git.

--- db/migrate/20130418133105_create_gyms_users_bridge_table.rb
+++ db/migrate/20130418133105_create_gyms_users_bridge_table.rb
@@ -1,4 +1,4 @@
-class CreateUserGyms < ActiveRecord::Migration
+class CreateGymsUsersBridgeTable < ActiveRecord::Migration
   def change
     create_table :user_gyms do |t|
       t.references :user
@@ -8,5 +8,6 @@ class CreateUserGyms < ActiveRecord::Migration
     end
     add_index :user_gyms, :user_id
     add_index :user_gyms, :gym_id
+    add_index :user_gyms, [ :gym_id, :user_id ], :unique => true
   end
 end
